823bcef8366b6bf17504aebcb60cc3712ef0dcf5
Igor Ratynski <igor.ratynski@MacBook-Pro-Igor.local>
2022-07-22 01:54:23 +0300

    delete all duplicated nodes

--- algorithms/Medium/LinkedList/LinkedListTasks.swift
+++ algorithms/Medium/LinkedList/LinkedListTasks.swift
@@ -10,16 +10,34 @@ import Foundation
 // MARK: - Delete duplicates
 extension Solution {
   func deleteDuplicates(_ head: ListNode?) -> ListNode? {
-    let result = head
-    var currentNode = result
+    let result = ListNode()
+    var lastValue: Int?
+    var currentNode: ListNode? = result
+    var head = head
+    
+    while head != nil {
+      if head?.val == head?.next?.val {
+        lastValue = head?.val
+      } else {
+        if head?.val != lastValue {
+          currentNode?.next = head
+          currentNode = currentNode?.next
+        }
+      }
+      
+      head = head?.next
+      currentNode?.next = nil
+    }
+
+    return result.next
   }
   
   func testDeleteDuplicates() {
-    //let a = ListNode(0, ListNode(0, ListNode(1, ListNode(1, ListNode(1)))))
+    let a =  ListNode(0, ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(3, ListNode(3)))))))
     var result: ListNode? = Solution().deleteDuplicates(a)
 
     while result != nil {
-     print("result: \(result?.val)")
+     print("result: \(result!.val)")
       result = result?.next
     }
   }
