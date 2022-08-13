3fc97c99e67fa15b2a51b95c4bc35d30c75def5b
Igor Ratynski <igor.ratynski@MacBook-Pro-Igor.local>
2022-08-13 03:49:34 +0300

    solved: medium: hash table: length of longest substring

---
+++ algorithms/Medium/HashMap/MediumHashMap.swift
@@ -0,0 +1,35 @@
+//
+//  MediumHashMap.swift
+//  Algorithms
+//
+//  Created by Igor Ratynski on 12.08.2022.
+//
+
+import Foundation
+
+// MARK: Length of longest substring
+extension Solution.Medium {
+  func lengthOfLongestSubstring(_ s: String) -> Int {
+    var result = 0, substring = Set<Character>(), tempCount = 0, stringArray: [Character] = s.reversed()
+
+    while !stringArray.isEmpty {
+      print("# \(stringArray)")
+      for index in stride(from: stringArray.count - 1, to: -1, by: -1) {
+        tempCount = substring.count
+        substring.insert(stringArray[index])
+
+        if substring.count == tempCount {
+          if substring.count > result {
+            print("substring: \(substring) count: \(substring.count)")
+            result = substring.count
+          }
+          substring.removeAll()
+          stringArray.removeLast()
+          break
+        }
+      }
+    }
+
+    return max(result, tempCount)
+  }
+}
