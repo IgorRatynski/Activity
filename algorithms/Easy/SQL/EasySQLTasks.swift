8bc317364c4cdf224dbe5dcea931645382af3a32
Igor Ratynski <igor.ratynski@MacBook-Pro-Igor.local>
2022-08-31 21:43:12 +0300

    solved: easy: sql: person with doubled email

---
+++ algorithms/Easy/SQL/EasySQLTasks.swift
@@ -0,0 +1,13 @@
+//
+//  EasySQLTasks.swift
+//  Easy
+//
+//  Created by Igor Ratynski on 31.08.2022.
+//
+
+import Foundation
+
+// MARK: Person with doubled email
+extension Solution.Easy {
+//  select email from Person group by email having count(*) > 1
+}
