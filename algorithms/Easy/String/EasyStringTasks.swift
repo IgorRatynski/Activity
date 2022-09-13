3f5b24d6580f142a2f78dfe7e418686be5553dc9
igor ratynski <igor.ratynski@gmail.com>
2022-09-13 07:33:58 +0300

    solved: 709. To Lower Case

--- algorithms/Easy/String/EasyStringTasks.swift
+++ algorithms/Easy/String/EasyStringTasks.swift
@@ -435,3 +435,14 @@ extension Solution.Easy {
       .joined(separator: " ")
   }
 }
+
+// MARK: 709. To Lower Case
+extension Solution.Easy {
+  func toLowerCase(_ s: String) -> String {
+    // I
+//    return s.lowercased()
+    // II
+    let alphabet: [Character:Character] = ["A":"a", "B":"b", "C":"c", "D":"d", "E":"e", "F":"f", "G":"g", "H":"h", "I":"i", "J":"j", "K":"k", "L":"l", "M":"m", "N":"n", "O":"o", "P":"p", "Q":"q", "R":"r", "S":"s", "T":"t", "U":"u", "V":"v", "W":"w", "X":"x", "Y":"y", "Z":"z"]
+    return String(s.map { alphabet[$0] != nil ? alphabet[$0]! : $0 })
+  }
+}
