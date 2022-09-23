52d80a6654addfc9b23f758759aca7ce0cc7943a
igor ratynski <igor.ratynski@gmail.com>
2022-09-23 21:11:09 +0300

    solved: 112. Path Sum

--- algorithms/Easy/LinkedList/EasyLinkedListTasks.swift
+++ algorithms/Easy/LinkedList/EasyLinkedListTasks.swift
@@ -141,3 +141,13 @@ extension Solution.Easy {
     node?.next = node?.next?.next
   }
 }
+
+// MARK: 112. Path Sum
+extension Solution.Easy {
+  func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+    guard let root = root else { return false }
+    if root.val == targetSum && root.left == nil && root.right == nil { return true }
+    
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+  }
+}
