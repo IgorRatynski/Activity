b3be5e8f7c64b0c90b408e152cef78baa7a9bf38
igor ratynski <igor.ratynski@gmail.com>
2022-10-09 22:18:20 +0300

    added tests: 507. Perfect Number

--- algorithms/TestAlgorithms/EasyTasks/TestEasyIntTasks.swift
+++ algorithms/TestAlgorithms/EasyTasks/TestEasyIntTasks.swift
@@ -10,6 +10,20 @@ import XCTest
 
 class TestEasyIntTasks: EasyTestCase { }
 
+// MARK: 507. Perfect Number
+extension TestEasyIntArrayTasks {
+  func testPerfectNumber() {
+    let inputs = [28, 1, 2, 3, 4, 5, 6, 7, 1000000, 9999999, 9999998, 9999997]
+    let outputs = [true, false, false, false, false, false, true, false, false, false, false, false]
+    let set = Set([inputs.count, outputs.count])
+    XCTAssertTrue(set.count == 1)
+    
+    for i in 0..<set.first! {
+      XCTAssertEqual(solution.checkPerfectNumber(inputs[i]), outputs[i])
+    }
+  }
+}
+
 // MARK: 258. Add Digits
 extension TestEasyIntArrayTasks {
   func testAddDigits() {
